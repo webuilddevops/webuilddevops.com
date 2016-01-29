f00515a47e0d9e6f90a67f1674664bb4129cc973
Douglas Land <dsl@looprock.com>
2016-01-28 22:51:49 -0600

    added search

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,13 +4,16 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Douglas Land'
 SITENAME = u'webuilddevops'
-SITEURL = 'https://webuilddevops.com'
+#SITEURL = 'https://webuilddevops.com'
+SITEURL = 'http://localhost:8000'
 COVER_IMG_URL = 'https://webuilddevops.com/images/snake-40427_640.png'
 PROFILE_IMAGE_URL = 'https://webuilddevops.com/images/snake-40427_640.png'
 TAGLINE = 'We build DevOps'
 
 PATH = 'content'
 STATIC_PATHS = ['images', 'extra/robots.txt', 'extra/favicon.ico', 'extra/apple-touch-icon-precomposed.png', 'extra/apple-touch-icon.png']
+
+#TEMPLATE_PAGES = {'pages/resources/vim.html': 'resources/vim.html', 'extras/search.html': 'search.html'}
 
 EXTRA_PATH_METADATA = {
     'extra/robots.txt': {'path': 'robots.txt'},
@@ -46,6 +49,7 @@ SOCIAL = (
     ('book', 'https://webuilddevops.com/pages/resources.html'),
     ('twitter-square', 'https://twitter.com/webuilddevops'),
     ('rss-square', 'https://webuilddevops.com/feeds/all.atom.xml'),
+    ('search', 'https://webuilddevops.com/search.html'),
 )
 
 DEFAULT_PAGINATION = 10
@@ -57,6 +61,12 @@ PLUGINS = [
     'pelican_youtube',
     'pelican_vimeo',
     'pelican_slideshare',
+    'tipue_search',
 ]
 
 THEME = "pure"
+#THEME = "plumage"
+
+TIPUE_SEARCH = True
+
+DIRECT_TEMPLATES = (('index', 'tags', 'categories', 'authors', 'archives', 'search'))
